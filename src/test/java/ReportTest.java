6f51d26f6168a3c00acb0c13a083ea36a7133662
Szymon Batko <szymonbatko91@gmail.com>
2021-06-23 00:22:24 +0200

    Set up the Test for Report 3, filtering in Report3 requires fix

--- src/test/java/ReportTest.java
+++ src/test/java/ReportTest.java
@@ -3,13 +3,12 @@ import com.umniedziala.reportgenerator.datamodel.Project;
 import com.umniedziala.reportgenerator.datamodel.Reports.ReportModel;
 import com.umniedziala.reportgenerator.datamodel.Task;
 import com.umniedziala.reportgenerator.services.report.Report1;
+import com.umniedziala.reportgenerator.services.report.Report3;
 import com.umniedziala.reportgenerator.storage.DataStorage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,6 +21,7 @@ public class ReportTest {
 
     @BeforeEach
     public void setUp() {
+        //dataStorage =  DataStorage.getInstance();
         dataStorage = new DataStorage();
         employee = null;
         project = null;
@@ -57,9 +57,40 @@ public class ReportTest {
 
         assertEquals(reportModel.getReportName(), "Report 1");
         LinkedList<ReportModel.Row> rows = reportModel.getRows();
-        assertEquals(rows.size(), 2);
+        assertEquals(rows.size(), 3);
         LinkedList<ReportModel.Cell> cells = rows.get(0).getCellsInRow();
         assertEquals(cells.size(), 3);
+    }
+    @Test
+    public void shouldGenerateReport3() {
+
+        Employee employee1 = new Employee("Szymon Batko");
+        Task task1 = new Task(new Date(2021-1900,01,01), "task1", 2.0);
+        Task task2 = new Task(new Date(2021-1900,02,28), "task2", 3.0);
+        ArrayList<Task> listOfTasks = new ArrayList<>();
+        listOfTasks.add(task1);
+        listOfTasks.add(task2);
+        Project project1 = new Project("Projekt1", listOfTasks);
+        employee1.add(project1);
+        HashSet<Employee> employees = new HashSet<>();
+        employees.add(employee1);
+       // dataStorage.setEmployees(employees);
+
+        Report3 report3 = new Report3();
+        ReportModel reportModel = report3.generateReport(dataStorage, "2012");
+
+        assertEquals("Report 3", reportModel.getReportName());
+        assertEquals("Miesiąc", reportModel.getRows().get(0).getCellsInRow().get(0).getValue());
+        assertEquals("Projekt", reportModel.getRows().get(0).getCellsInRow().get(1).getValue());
+        assertEquals("Godziny", reportModel.getRows().get(0).getCellsInRow().get(2).getValue());
+
+        ReportModel.Cell cell = reportModel.getRows().get(1).getCellsInRow().get(0);
+        assertEquals("1", cell.getValue());
+        cell = reportModel.getRows().get(1).getCellsInRow().get(1);
+        assertEquals("Projekt1", cell.getValue());
+        cell =reportModel.getRows().get(1).getCellsInRow().get(2);
+        assertEquals("2.0", cell.getValue());
+
     }
 
 }
